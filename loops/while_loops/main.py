6dbac716e994e7e20c8d6edeaf34a8dca5548509
dogra1495-afk <>
2026-06-19 10:15:57 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,14 @@
+start_number = 5
+countdown_values = []
+
+while start_number > 0:
+    countdown_values.append(start_number)
+    print(F"Discount countdown complete! {countdown_values}")
+    start_number -= 1
+
+
+print(F"Discount countdown complete! {countdown_values}")
+
+
+
+
